175e94e552fb4fcb75c81c20a38985e6415d6bab
rparker24 <robert@robertmparker.net>
2016-06-25 08:55:49 -0400

    setup DB

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 
@@ -14,12 +15,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// DB setup
+mongoose.connect('mongodb://localhost/nytreact');
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+console.log('Mongoose Error: ', err);
+});
+db.once('open', function () {
+console.log('Mongoose connection successful.');
+});
+
+//Require Schemas
+var Article = require('./models/Article.js');
+
 
 app.use('/', routes);
 
